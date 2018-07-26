4f5c69b6cb99f253dbf9d53b9c492b52021af57b
coder <mshawnlouw@gmail.com>
2018-07-26 13:17:04 +0200

    few changes made in 2 files

--- test/findItemsOver20.test.js
+++ test/findItemsOver20.test.js
@@ -1,9 +1,6 @@
 let assert = require('assert');
-<<<<<<< HEAD
-let findItemsOver20 = require('../findItemsOver20')
-=======
+
 let findItemsOver20 = require('../findItemsOver20');
->>>>>>> 1161fdd730e86b8a2fa99519fa1d13e86ba26229
 
 describe('The findItemsOver20 function', function(){
 
@@ -13,18 +10,6 @@ describe('The findItemsOver20 function', function(){
     {name : 'pears', qty : 37},
     {name : 'bananas', qty : 27},
     {name : 'apples', qty : 3},
-      ]))
+  ]));
   })
-<<<<<<< HEAD
-=======
-
-  // it('should return the names of products in the list that are over20', function(){
-  //   assert.equal("bananas, pears", findItemsOver20([
-  //   {name : 'apples', qty : 10},
-  //   {name : 'pears', qty : 37},
-  //   {name : 'bananas', qty : 27},
-  //   {name : 'apples', qty : 3},
-  //     ]))
-  // })
->>>>>>> 1161fdd730e86b8a2fa99519fa1d13e86ba26229
-})
+});
